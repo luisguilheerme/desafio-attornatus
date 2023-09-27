b9b3ea797763fdf5fe6b3672c75e52493b917e83
luisguilheerme <lg.maroni@gmail.com>
2023-09-27 13:39:21 -0300

    Project created

---
+++ src/test/java/com/luisguilherme/desafioattornatus/DesafioAttornatusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luisguilherme.desafioattornatus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioAttornatusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
